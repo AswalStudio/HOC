f06f8081c1575d2a8273ecd227a48760d597830d
Trilok Mehta <trilokmehta404@gmail.com>
2026-03-16 23:42:47 +0530

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,19 +1,22 @@
 class AppFooter extends HTMLElement {
   connectedCallback() {
-    // Get the current year automatically (e.g., 2026, 2027...)
+    // Get the current year automatically
     const currentYear = new Date().getFullYear();
 
     this.innerHTML = `
       <footer id="contact">
         <div class="footer-content">
+          
           <div class="footer-col">
             <h4>House of Charming</h4>
             <p style="margin-bottom: 16px">Premium Product Catalog</p>
             <ul class="footer-links">
-              <li>GST: 27AAAAA0000A1Z5</li>
-              <li>CIN: U74999MH2010PTC000000</li>
+              <li><a href="https://www.instagram.com/charminghomeaids/" target="_blank" rel="noopener noreferrer">Instagram Profile</a></li>
+              <li>GSTIN: 07APNPS0454A1Z1</li>
+              <li>Legal Name: OMKAR SAXENA</li>
             </ul>
           </div>
+          
           <div class="footer-col">
             <h4>Quick Links</h4>
             <ul class="footer-links">
@@ -23,17 +26,20 @@ class AppFooter extends HTMLElement {
               <li><a href="#">Privacy Policy</a></li>
             </ul>
           </div>
+          
           <div class="footer-col">
             <h4>Contact Us</h4>
             <ul class="footer-links">
               <li>
-                <a href="mailto:info@houseofcharming.com">houseofcharming1991@gmail.com</a>
+                <a href="mailto:houseofcharming1991@gmail.com">houseofcharming1991@gmail.com</a>
               </li>
               <li><a href="tel:+919811017918">+91 9811017918</a></li>
               <li>M26R+Q26, Rana Ji Enclave, Ranhola, Delhi, 110041</li>
             </ul>
           </div>
+          
         </div>
+        
         <div class="footer-bottom">
           <p>&copy; ${currentYear} House of Charming. All Rights Reserved.</p>
         </div>
